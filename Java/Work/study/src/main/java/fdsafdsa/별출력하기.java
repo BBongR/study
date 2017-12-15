0dde158eb0df96a7e92d68d4f7239b9d5b2da43f
BBongR <suv1214@naver.com>
2017-12-15 19:22:48 +0900

    1

--- Java/Work/study/src/main/java/fdsafdsa/별출력하기.java
+++ Java/Work/study/src/main/java/fdsafdsa/별출력하기.java
@@ -3,38 +3,17 @@ package fdsafdsa;
 public class 별출력하기 {
 	public static void main(String[] args) {
 
-		// for (int i = 0; i < 5; i++) {
-		// for (int j = 0; j < 10; j++) {
-		// System.out.print("*");
-		// }
-		// System.out.println();
-		// }
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (j <= i) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
 
-		// for (int i = 0; i < 10; i++) {
-		// for (int j = 0; j < 10; j++) {
-		// System.out.print("*");
-		// }
-		// System.out.println();
-		// }
-
-//		for (int i = 0; i < 10; i++) {
-//			for (int j = 0; j <= i; j++) {
-//				System.out.print("*");
-//			}
-//			System.out.println();
-//		}
-
-		int k = 0;
-		int z = 0;
-
-		while (k < 10) {
-			while (z <= k) {
-				System.out.print("*");
-				z++;
 			}
-			k++;
 			System.out.println();
 		}
-	}
 
+	}
 }
