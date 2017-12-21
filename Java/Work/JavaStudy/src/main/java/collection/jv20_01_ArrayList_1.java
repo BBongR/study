0ccdfd63af146f67392e43327e62dedc11acba7e
BBongR <suv1214@naver.com>
2017-12-21 18:10:36 +0900

    1

--- Java/Work/JavaStudy/src/main/java/collection/jv20_01_ArrayList_1.java
+++ Java/Work/JavaStudy/src/main/java/collection/jv20_01_ArrayList_1.java
@@ -2,7 +2,7 @@ package collection;
 
 import java.util.*;
 
-public class jv20_01_ArrayList {
+public class jv20_01_ArrayList_1 {
 
 	public static void main(String[] args) {
 
@@ -18,6 +18,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * C: 추가. 검색: "자바 arraylist 추가" MILK, BREAD, BUTTER 순으로 추가
 		 */
+
 		list.add("MILK");
 		list.add("BREAD");
 		list.add("BUTTER");
@@ -28,6 +29,8 @@ public class jv20_01_ArrayList {
 		 * APPLE 삽입. 검색: "자바 arraylist 삽입" 특정 위치에 추가하기 "BREAD" 앞에 "APPLE" 삽입 "BREAD" 가
 		 * 들어있는 방번호 찾기
 		 */
+
+		// list.add(Collections.binarySearch(list, "BREAD"), "APPLE");
 		list.add(list.indexOf("BREAD"), "APPLE");
 		System.out.println("C: BREAD 앞에 APPLE 삽입 >> " + list);
 
@@ -35,13 +38,15 @@ public class jv20_01_ArrayList {
 		/*
 		 * R: 읽기 BUTTER 값을 출력하시오. "BUTTER" 가 들어있는 방번호 찾기
 		 */
-		System.out.println(list.indexOf("BUTTER"));
+
+		value = String.valueOf(list.indexOf("BUTTER"));
 		System.out.println("R: 읽기 >> " + value);
 
 		System.out.println("--------------도전 4--------------");
 		/*
 		 * U: 수정. 검색: "자바 arraylist 수정" "BREAD" 를 "GRAPE"로 변경 "BREAD" 가 들어있는 방번호 찾기
 		 */
+
 		list.set(list.indexOf("BREAD"), "GRAPE");
 		System.out.println("U: BREAD 를 GRAPE로 수정 >> " + list);
 
@@ -49,6 +54,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * D: 인덱스로 삭제. 검색: "자바 arraylist 삭제" 인덱스를 이용하여 GRAPE 를 삭제
 		 */
+
 		list.remove(list.indexOf("GRAPE"));
 		System.out.println("D: BUTTER 를 삭제 >> " + list);
 
@@ -56,6 +62,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * D: 값으로 찾아서 삭제. 검색: "자바 arraylist 값으로 삭제" 값으로 MILK를 찾아서 삭제하시오
 		 */
+
 		list.remove("MILK");
 		System.out.println("D: 값으로 MILK를 찾아서 삭제 >> " + list.toString());
 
@@ -63,6 +70,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * P: 리스트를 for문으로 출력. 검색: "자바 arraylist for 출력" 검색: "자바 arraylist 크기"
 		 */
+
 		System.out.print("P: 리스트를 for문으로 출력 : ");
 		for (int i = 0; i < list.size(); i++) {
 			System.out.print(list.get(i) + ", ");
@@ -73,6 +81,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * P: 리스트를 for-each문으로 출력.
 		 */
+
 		System.out.print("P: 리스트를 for-each문으로 출력 : ");
 		for (String string : list) {
 			System.out.print(string + ", ");
@@ -90,6 +99,7 @@ public class jv20_01_ArrayList {
 		 * S: 검색: "자바 ArrayList 검색 첫번째 APPLE을 찾으시오. List<String> 선언한 경우
 		 * Collections.binarySearch() 오작동.
 		 */
+        System.out.println(list);
 		pos = list.indexOf("APPLE");
 		System.out.println("APPLE 검색 : " + pos);
 
@@ -97,6 +107,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * S: 오름차순 정렬. 검색: "자바 ArrayList 오름차순 정렬"
 		 */
+
 		Collections.sort(list);
 		System.out.println("S: 오름차순 정렬 >> " + list.toString());
 
@@ -104,6 +115,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * S: 내림차순 정렬. 검색: "자바 ArrayList 내림차순 정렬"
 		 */
+
 		Collections.reverse(list);
 		System.out.println("S: 내림차순 정렬 >> " + list.toString());
 
@@ -111,6 +123,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * 검색2. APPLE 이 몇개 있나요?
 		 */
+
 		int count = 0;
 		count = list.indexOf("APPLE");
 		System.out.println("S: APPLE 이 몇개 있나요? >> " + count);
@@ -119,16 +132,18 @@ public class jv20_01_ArrayList {
 		/*
 		 * 리스트를 배열로 변환하시오. toArray() 사용 "자바 ArrayList 배열 변환 검색"
 		 */
-		String[] str = new String[list.size()];
-		list.toArray(str);
+
+		String[] s = new String[list.size()];
+		list.toArray(s);
 
 		System.out.println("--------------도전 14--------------");
 		/*
 		 * 변환된 배열을 for 문으로 출력.
 		 */
+
 		System.out.print("P : 변환 결과를 for 문으로 출력 ");
-		for (int i = 0; i < str.length; i++) {
-			System.out.print(str[i] + ", ");
+		for (int i = 0; i < s.length; i++) {
+			System.out.print(s[i] + ", ");
 		}
 
 		System.out.println();
@@ -136,8 +151,9 @@ public class jv20_01_ArrayList {
 		/*
 		 * 변환된 배열을 for each 문으로 출력.
 		 */
+
 		System.out.print("P : 변환 결과를 for each 문으로 출력 : ");
-		for (String string : str) {
+		for (String string : s) {
 			System.out.print(string + ", ");
 		}
 
@@ -146,6 +162,7 @@ public class jv20_01_ArrayList {
 		/*
 		 * list의 모든 값을 삭제하시오
 		 */
+
 		list.removeAll(list);
 		System.out.println("D: list의 모든 값을 삭제하시오 >> " + list);
 
---
+++ Java/Work/JavaStudy/src/main/java/collection/jv20_03_HashMap_1.java
@@ -0,0 +1,124 @@
+package collection;
+
+import java.util.*;
+
+public class jv20_03_HashMap_1 {
+	public static void main(String[] args) {
+
+		Map<String, String> map = null;
+
+		map = new HashMap<String, String>();
+		// map = new Hashtable<String, String>();
+		// map = new TreeMap<String, String>();
+
+		System.out.println("--------------도전 1--------------");
+		/*
+		 * C: 추가. 검색: "자바 HashMap 추가"
+		 *
+		 * 송중기 이광수 송혜교
+		 */
+
+		map.put("0", "송중기");
+		map.put("1", "이광수");
+		map.put("2", "송혜교");
+		System.out.println(" C: 추가 >> " + map.toString()); // 모든 항목을 출력한다.
+
+		System.out.println("--------------도전 2--------------");
+		/*
+		 * C: 삽입. 검색: "자바 HashMap 삽입"
+		 *
+		 * 유재석 삽입 되나 안되나? X
+		 */
+
+		map.put("1", "유재석");
+		System.out.println(map.toString()); // 모든 항목을 출력한다.
+
+		System.out.println("--------------도전 3--------------");
+		/*
+		 * R: 읽기
+		 *
+		 * 송혜교만 출력
+		 */
+
+		System.out.println(map.get("2"));
+		System.out.println(map.containsValue("송혜교"));
+
+		System.out.println("--------------도전 4--------------");
+		/*
+		 * U: 수정.변경. 검색: "자바 HashMap 수정"
+		 *
+		 * 이광수를 하하로 변경
+		 */
+
+		map.replace("1", "하하");
+		// map.replace("1", "유재석", "하하");
+		System.out.println(map.toString());
+
+		System.out.println("--------------도전 5--------------");
+		/*
+		 * D: 키 값으로 삭제. 검색: "자바 HashMap 삭제"
+		 *
+		 * 송혜교를 삭제
+		 */
+
+		map.remove("2");
+		// map.remove("2", "송혜교");
+		System.out.println(map.toString());
+
+		System.out.println("--------------도전 6--------------");
+		// for문으로 Map 출력하기. 방법1
+		// iterator()를 이용하는 방식은 루프안에서 map 데이터를 삭제할때 사용.
+		// http://stove99.tistory.com/96
+
+		for (int i = 0; i < map.size(); i++) {
+			System.out.print(map.get(String.valueOf(i)) + ", ");
+		}
+
+		System.out.println();
+
+		System.out.println("--------------도전 7--------------");
+		// foreach문으로 Map 출력하기. 방법2
+		// keySet() 보다 빠르다.
+		// http://stove99.tistory.com/96
+
+		for (String key : map.keySet()) {
+			System.out.printf("키:%s, 값:%s\n", key, map.get(key));
+		}
+
+		System.out.println();
+		System.out.println("--------------도전 8--------------");
+		// foreach문으로 Map 출력하기. 방법3
+		// http://stove99.tistory.com/96
+
+		for (String values : map.values()) {
+			System.out.print(values + ", ");
+		}
+
+		System.out.println();
+		System.out.println("--------------도전 9--------------");
+		// S: HashMap 검색. "자바 HashMap 검색"
+		// S: 키로 검색 containsKey() 사용
+		System.out.println(map.containsKey("1"));
+
+		System.out.println("--------------도전 10--------------");
+		// S: 값으로 검색 containsValue() 사용
+		System.out.println(map.containsValue("하하"));
+
+		// S: HashMap 오름차순 정렬. 검색: "자바 HashMap 오름차순 정렬"
+		// http://huskdoll.tistory.com/5/
+
+		// S: HashMap 내림차순 정렬. 검색: "자바 HashMap 내림차순 정렬"
+		// http://huskdoll.tistory.com/5
+		TreeMap<String, String> treeMapReverse = new TreeMap<String, String>(Collections.reverseOrder());
+		treeMapReverse.putAll(map);
+
+		Iterator<String> treeMapReverseIter = treeMapReverse.keySet().iterator();
+		for (; treeMapReverseIter.hasNext();) {
+
+			String key = treeMapReverseIter.next();
+			String value = treeMapReverse.get(key);
+
+			System.out.println(key + " : " + value);
+		}
+	}
+}
